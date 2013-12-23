e4b4d2922ea172cc71f504bed11a89b3cce265a4
vukicevic <vukicevic@gmail.com>
2013-12-24 00:53:13 +0100

    app changes

--- resources/examples.js
+++ resources/examples.js
@@ -10,43 +10,47 @@ function RouterApp(stdout, prompt, caller) {
   stdout("Terminal active. Type 'keygen' to generate RSA keys.");
   return function process(stdin) {
     switch (stdin) {
-    case undefined:
-      prompt("");
-      return process;
     case "keygen":
       return KeyGenApp.entry(stdout, prompt, process);
     default:
-      stdout("Unknown command.");
+      stdout("Unknown command");
       return process;
     }
   }
 }
 
 var KeyGenApp = {
+  exit: function(stdout, prompt, caller) {
+    stdout("Goodbye", " ");
+    prompt("");
+    return caller;
+  },
+
   entry: function(stdout, prompt, caller) {
+    stdout(" ", "Welcome to the RSA keygen app");
+    prompt("keygen");
+    return KeyGenApp.enterType(stdout, prompt, caller);
+  },
+
+  enterType: function(stdout, prompt, caller) {
     function print() {
-      stdout(" ", "Welcome to the RSA keygen app.", " ", "Do you wish to generate and export an OpenPGP style key, or just generate the key?", " ");
-      stdout("1) Key Generation and OpenPGP Key Export", "2) Key Generation", " ");
+      stdout(" ", "Do you wish to generate and export an OpenPGP style key, or just generate the key?", " ");
+      stdout("1) Key Generation");
+      stdout("2) Key Generation and OpenPGP Key Export", " ");
     }
 
-    prompt("keygen");
     print();
 
     return function process(stdin) {
-      if (stdin == 1)
-        return KeyGenApp.enterName(stdout, prompt, caller);
       if (stdin == 2)
-        return KeyGenApp.enterKeySize(stdout, prompt, caller);
+        return KeyGenApp.enterName(stdout, prompt, caller);
+      if (stdin == 1)
+        return KeyGenApp.enterSize(stdout, prompt, caller);
 
-      stdout(" ", "Invalid option. Please select one of the presented options.", " ");
+      stdout(" ", "Invalid input. Please select one of the presented options", " ");
       print();
       return process;
     }
-  },
-
-  exit: function(stdout, prompt, caller) {
-    stdout("Goodbye");
-    return caller();
   },
 
   enterName: function(stdout, prompt, caller) {
@@ -58,15 +62,15 @@ var KeyGenApp = {
 
     return function process(stdin) {
       if (stdin.length > 2 && stdin.length < 250)
-        return KeyGenApp.enterKeySize(stdout, prompt, caller);
+        return KeyGenApp.enterSize(stdout, prompt, caller);
 
-      stdout(" ", "Invalid input. Name must be between 3 and 250 characters long.", " ");
+      stdout(" ", "Invalid input. Name must be between 3 and 250 characters long");
       print();
       return process;
     }
   },
 
-  enterKeySize: function(stdout, prompt, caller) {
+  enterSize: function(stdout, prompt, caller) {
     function print() {
       stdout(" ", "Enter a key size in bits, e.g. 1024, 1536, 2048", " ");
     }
